7efd82cf8dfcae8ed297431cf27ce5c940a8bb2f
frquiros <franqm95@gmail.com>
2019-03-26 15:13:06 -0600

    added php components

---
+++ adminFooter.php
@@ -0,0 +1,13 @@
+</section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.0
+    </div>
+    <strong>Copyright &copy; 2019 <a href="">SIRA</a>.</strong> All rights reserved.
+  </footer>
+
+</div>
+<!-- ./wrapper -->
